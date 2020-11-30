0c5424ef8078bf46f30d04635f21507c5ec6cf3e
Joel Wall <s3548001@student.rmit.edu.au>
2020-12-01 00:45:58 +1100

    Added Rocket explosion animation and sound

--- Assets/Gun/Collision.cs
+++ Assets/Gun/Collision.cs
@@ -8,6 +8,7 @@ public class Collision : MonoBehaviour
     public GameObject terrain;
     public float TextureDamageRadius = 22f;
     public float TerrainDamageRadius = 22f;
+    public GameObject explosion;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,9 @@ public class Collision : MonoBehaviour
         {
 
             terrain.GetComponent<TerrainDeformer>().DestroyTerrain(c.contacts[0].point, TerrainDamageRadius, TextureDamageRadius);
+            GameObject explosionClone = Instantiate(explosion, c.contacts[0].point, Quaternion.Euler(new Vector3(0, 0, 0)));
+
+            Destroy(explosionClone, 2);
             Destroy(this.gameObject);
         }
     }
