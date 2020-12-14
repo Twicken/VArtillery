93fb883ca95a029212f350edabbf29a7aad3c9b8
Joel Wall <s3548001@student.rmit.edu.au>
2020-12-14 23:14:27 +1100

    hand animation now happens over network

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -11,6 +11,10 @@ public class NetworkPlayer : MonoBehaviour
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
     private PhotonView photonView;
     
     private Transform headRig;
@@ -26,6 +30,15 @@ public class NetworkPlayer : MonoBehaviour
         leftHandRig  = rig.transform.Find("Camera Offset/Left Hand");
         rightHandRig = rig.transform.Find("Camera Offset/Right Hand");
 
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
+
+
     }
 
     // Update is called once per frame
@@ -34,14 +47,46 @@ public class NetworkPlayer : MonoBehaviour
         if (photonView.IsMine)
         {
 
-            rightHand.gameObject.SetActive(false);
-            leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
-             MapPosition(head, headRig);
-                    MapPosition(leftHand, leftHandRig);
-                    MapPosition(rightHand, rightHandRig);
+            //rightHand.gameObject.SetActive(false);
+            //leftHand.gameObject.SetActive(false);
+            //head.gameObject.SetActive(false);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
         }
        
+    }
+
+
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+
+            handAnimator.SetFloat("Grip", 0);
+        }
+
     }
 
     void MapPosition(Transform target, Transform rigTransform)
